03b5efb17a63da0d74793fef8991e9ee9bdc6f73
ripassorker2 <ripassorkar08@gmail.com>
2024-02-11 20:51:17 +0600

    fixd route  issue

--- src/pages/Dashboard/Profile.jsx
+++ src/pages/Dashboard/Profile.jsx
@@ -42,6 +42,7 @@ const Profile = () => {
                 .then((data) => setAllMoods(data?.data));
         }
     }, []);
+    console.log(allMoods?.length);
 
     useEffect(() => {
         setAnxious(findMood(allMoods, "Anxious"));
@@ -54,81 +55,87 @@ const Profile = () => {
     return (
         <div className="mb-10">
             <div className="grid md:grid-cols-2 gap-10">
-                <motion.div
-                    whileInView={{opacity: [0, 1], y: [0, -20]}}
-                    transition={{duration: 0.5, delay: 0.4}}
-                    initial={{opacity: 0}}
-                    className="">
-                    <div className="flex justify-center items-center">
-                        <Chart
-                            options={{
-                                labels: [
-                                    "Anxious",
-                                    "Stressed",
-                                    "Happy",
-                                    "Angry",
-                                    "Sad",
-                                ],
-                                legend: {
-                                    fontSize: "16px",
-                                    labels: {
-                                        colors: "#f1f1f1",
-                                        useSeriesColors: false,
-                                    },
-                                    horizontalAlign: "center",
-
-                                    position: "bottom",
-                                },
-                                theme: {
-                                    mode: "light",
-                                    palette: "palette",
-                                    monochrome: {
-                                        enabled: false,
-                                        color: "#255aee",
-                                        shadeTo: "light",
-                                        shadeIntensity: 0.65,
-                                    },
-                                },
-                                responsive: [
-                                    {
-                                        breakpoint: 978,
-                                        options: {
-                                            chart: {
-                                                width: 380,
+                {allMoods?.length ? (
+                    <>
+                        <motion.div
+                            whileInView={{opacity: [0, 1], y: [0, -20]}}
+                            transition={{duration: 0.5, delay: 0.4}}
+                            initial={{opacity: 0}}
+                            className="">
+                            <div className="flex justify-center items-center">
+                                <Chart
+                                    options={{
+                                        labels: [
+                                            "Anxious",
+                                            "Stressed",
+                                            "Happy",
+                                            "Angry",
+                                            "Sad",
+                                        ],
+                                        legend: {
+                                            fontSize: "16px",
+                                            labels: {
+                                                colors: "#f1f1f1",
+                                                useSeriesColors: false,
                                             },
-                                            legend: {
-                                                position: "bottom",
+                                            horizontalAlign: "center",
+
+                                            position: "bottom",
+                                        },
+                                        theme: {
+                                            mode: "light",
+                                            palette: "palette",
+                                            monochrome: {
+                                                enabled: false,
+                                                color: "#255aee",
+                                                shadeTo: "light",
+                                                shadeIntensity: 0.65,
                                             },
                                         },
-                                    },
-                                    {
-                                        breakpoint: 480,
-                                        options: {
-                                            chart: {
-                                                width: 300,
+                                        responsive: [
+                                            {
+                                                breakpoint: 978,
+                                                options: {
+                                                    chart: {
+                                                        width: 380,
+                                                    },
+                                                    legend: {
+                                                        position: "bottom",
+                                                    },
+                                                },
                                             },
-                                            legend: {
-                                                position: "bottom",
+                                            {
+                                                breakpoint: 480,
+                                                options: {
+                                                    chart: {
+                                                        width: 300,
+                                                    },
+                                                    legend: {
+                                                        position: "bottom",
+                                                    },
+                                                },
                                             },
-                                        },
-                                    },
-                                ],
-                            }}
-                            series={[
-                                anxious?.length ? anxious.length + 1 : 1,
-                                stressed?.length ? stressed.length + 1 : 1,
-                                happy?.length ? happy.length + 1 : 1,
-                                angry?.length ? angry.length + 1 : 1,
-                                sad?.length ? sad.length + 1 : 1,
-                            ]}
-                            type="pie"
-                            width={400}
-                            height={350}
-                        />
-                    </div>
-                </motion.div>
+                                        ],
+                                    }}
+                                    series={[
+                                        anxious?.length,
+                                        stressed?.length,
+                                        happy?.length,
+                                        angry?.length,
+                                        sad?.length,
+                                    ]}
+                                    type="pie"
+                                    width={400}
+                                    height={350}
+                                />
+                            </div>
+                        </motion.div>
+                    </>
+                ) : (
+                    <></>
+                )}
 
-                <div className="">
+                <div>
                     <Emotions
                         stressed={stressed}
                         anxious={anxious}
@@ -137,27 +144,26 @@ const Profile = () => {
                         sad={sad}
                     />
                 </div>
-                <div>
-                    <motion.div
-                        whileInView={{opacity: [0, 1], y: [0, -20]}}
-                        transition={{duration: 0.5, delay: 0.4}}
-                        initial={{opacity: 0}}
-                        className="rounded-lg text-gray-200 flex justify-center items-center bg-primary py-12 px-4 relative mt-6">
-                        <div>
-                            <p className="text-center font-semibold text-2xl">
-                                {journals?.length ? journals.length : 0}
-                            </p>
-                            <h2 className="text-2xl mb-3 font-semibold">
-                                Your Journals
-                            </h2>
-                            <Link
-                                to={"/dashboard/add-journal"}
-                                className="btn-primary text-lg">
-                                Add journal
-                            </Link>
-                        </div>
-                    </motion.div>
-                </div>
+
+                <motion.div
+                    whileInView={{opacity: [0, 1], y: [0, -20]}}
+                    transition={{duration: 0.5, delay: 0.4}}
+                    initial={{opacity: 0}}
+                    className="rounded-lg text-gray-200 flex justify-center items-center bg-primary py-14 px-4 relative mt-10">
+                    <div>
+                        <p className="text-center font-semibold text-2xl">
+                            {journals?.length ? journals.length : 0}
+                        </p>
+                        <h2 className="text-xl mb-3 font-semibold">
+                            Your Journals
+                        </h2>
+                        <Link
+                            to={"/dashboard/add-journal"}
+                            className="btn-primary py-2 text-lg">
+                            Add journal
+                        </Link>
+                    </div>
+                </motion.div>
             </div>
         </div>
     );
